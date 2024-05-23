8bcac90f890b1718e25f931f74bc401a5e9d55f8
Esosa <esosa.agbonavbare@decagon.dev>
2024-05-23 19:31:51 +0100

    Designed home page

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,13 @@
 import starrynight from './background/starrynight.png';
 import light from './background/light.png';
 import ascend from './home/ascend.png';
+import raw from './home/raw.png';
+import container from './home/Container.png';
 
 export {
     starrynight,
     light,
-    ascend
+    ascend,
+    raw,
+    container
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { light } from '../assets';
+import { light, ascend, raw, container } from '../assets';
 
 const Home = () => {
   return (
@@ -19,8 +19,8 @@ const Home = () => {
             </button>
         </nav>
         <div className="flex flex-col items-center justify-center text-white text-center">
-            <h1 className="text-4xl font-bold font-maven-pro">Igniting Your Passion for Movies, <br /> Feel the Pulse!</h1>
-            <p className="text-lg mt-4 font-rubik">
+            <h1 className="text-2xl sm:text-4xl font-bold font-maven-pro">Igniting Your Passion for Movies, <br /> Feel the Pulse!</h1>
+            <p className="text-sm sm:text-lg mt-4 font-rubik">
                 Welcome to CinePulse, where the screen comes alive, offering a captivating web app <br />
                 experience that fuels your love for movies.
             </p>
@@ -30,6 +30,23 @@ const Home = () => {
                 Discover CinePulse
             </button>
         </div>
+        <img 
+            src={ascend}
+            alt='movie-ascend'
+            className='absolute hidden sm:inline-block sm:top-[205px] sm:right-0 sm:w-[200px] sm:h-[256px]'
+        />
+        <img 
+            src={raw}
+            alt='movie-raw'
+            className='absolute hidden sm:inline-block sm:top-[205px] sm:left-0 sm:w-[200px] sm:h-[256px]'
+        />
+        <section className='relative h-[50%] sm:h-[52%] overflow-hidden mt-4'>
+            <img 
+                src={container}
+                alt='img-container'
+                className='absolute top-[-21px] h-[350px] sm:top-[-1px] sm:left-[150px] sm:w-[978px] sm:h-[570px] rounded-[8px]'
+            />
+        </section>
     </div>
   )
 };
