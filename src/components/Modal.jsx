f8ef1ae1f6bb7a4014e7196afaef49dae6e7e5a2
Esosa <esosa.agbonavbare@decagon.dev>
2024-05-27 14:07:18 +0100

    Added movie detail changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ isOpen, onClose, movie }) => {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 overflow-auto">
-      <div className="bg-white rounded-lg overflow-hidden shadow-lg max-w-lg w-full">
+      <div className="bg-white rounded-lg overflow-hidden pt-14 shadow-lg max-w-lg w-full">
         <div className="relative">
           <button 
             className="absolute top-[100px] right-[20px] mt-1 mr-2 text-black font-bold z-[60] bg-white rounded-full w-10 h-10 flex items-center justify-center text-3xl"
@@ -13,7 +13,7 @@ const Modal = ({ isOpen, onClose, movie }) => {
           >
             &times;
           </button>
-          <img src={movie.poster} alt={movie.Title} className="w-full h-[500px]"/>
+          <img src={movie.poster} alt={movie.Title} className="w-full pt-14 h-[500px]"/>
         </div>
         <div className="p-4">
           <h2 className="text-2xl font-bold mb-2">{movie.title}</h2>
